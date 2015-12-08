320e770b48c39a49ac408b640357a75557777f06
kmein <kieran.meinhardt@gmail.com>
2015-12-08 22:45:44 +0100

    Komplette Verzeichnisstruktur verändert. Design erneuert.

---
+++ js/main.js
@@ -0,0 +1,36 @@
+var news = {
+    'DE': [
+        "Troja: Bevölkerung auf dem Holzweg", 
+        "Sokrates: er weiß, dass er nichts weiß", 
+        "Hokus Pokus Phidias: Zeusstatue in Olympia fertiggestellt"
+     ],
+    'LA': [
+        "Tróia: cives in via lígnea", 
+        "Sócrates: scit se nescire", 
+        "Hocus pocus Phídias: simulacrum Iovis confectum in Olýmpia"
+    ],
+    'GR': [
+        "Τροία: αἱ πολιταὶ ἐπὶ ὁδῷ ξυλινῷ", 
+        "Σωκράτης: ἓν οἶδεν ὄτι οὐδὲν οἶδεν", 
+        "Ὅκος πόκος Φειδίας: εἴδολον τοῦ Διὸς διαπέπρακται ἐν τῇ Ὀλυμπίᾳ"
+    ]
+};
+
+function getRandom(array) {
+    var randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
+}
+
+function getNewsText(language) {
+    return '+++ ' + getRandom(news[language]) + ' +++';
+}
+
+function setMarquee(language) {
+    var marquee = document.getElementsByTagName("marquee")[0]
+    marquee.firstChild.nodeValue = getNewsText(language);
+    marquee.start();
+}
+
+function init(language) {
+    setMarquee(language);
+}
--- texts.md
+++ texts.md
@@ -2,6 +2,13 @@
 
 ## Hauptseite
 [Slideshow der einzelnen Rubriken]
+
+Bild	   Titel	       Text
+----       -----               ----
+Zeusstatue τὰ Θεῖα             μάθετε περὶ πάντων τῶν Ὀλυμπικῶν θεῶν
+Zeichnung  ἡ Τέχνη καὶ ἡ Σκευή τὶ ἔστι καινὸν ἐν Ἑλλαδί;
+
+
 Willkommen auf der Website der altgriechischen Regierung
 
 ## Kunst und Mode
