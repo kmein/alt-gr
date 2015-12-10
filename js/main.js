8dcc7b8f307e9bacd62ad2acc68a63c2f2fadbc1
kmein <kieran.meinhardt@gmail.com>
2015-12-10 20:05:38 +0100

    Sehenswertes-Seite angefangen. Neue Bilder hinzugefügt. Favicon hinzugefügt

--- js/main.js
+++ js/main.js
@@ -2,17 +2,23 @@ var news = {
     'DE': [
         "Troja: Bevölkerung auf dem Holzweg", 
         "Sokrates: er weiß, dass er nichts weiß", 
-        "Hokus Pokus Phidias: Zeusstatue in Olympia fertiggestellt"
+        "Hokus Pokus Phidias: Zeusstatue in Olympia fertiggestellt",
+        "Platon: 'Atlantis 480 BC' läuft in den Kinos an",
+        "Sokrates: zum Tode verurteilt"
      ],
     'LA': [
         "Tróia: cives in via lígnea", 
         "Sócrates: scit se nescire", 
-        "Hocus pocus Phídias: simulacrum Iovis confectum in Olýmpia"
+        "Hocus pocus Phídias: simulacrum Iovis confectum in Olýmpia",
+        "Plato: 'Atlantia 480 a. Chr. n.' incípitur in theatris",
+        "Sócrates: capitis est damnatus"
     ],
     'GR': [
-        "Τροία: αἱ πολιταὶ ἐπὶ ὁδῷ ξυλινῷ", 
+        "Τροία: αἱ πολιταὶ ἐφ' ὁδῷ ξυλινῷ", 
         "Σωκράτης: ἓν οἶδεν ὄτι οὐδὲν οἶδεν", 
-        "Ὅκος πόκος Φειδίας: εἴδωλον τοῦ Διὸς διαπέπρακται ἐν τῇ Ὀλυμπίᾳ"
+        "Ὅκος πόκος Φειδίας: εἴδωλον τοῦ Διὸς διαπέπρακται ἐν τῇ Ὀλυμπίᾳ",
+        "Πλάτων: 'Ἀτλαντίς 480 π. Χρ.' ἄρχεται ἐν τοῖς θεάτροις",
+        "Σωκράτης: κατεγνοῦται θανάτου"
     ]
 };
 
@@ -21,9 +27,7 @@ function getRandom(array) {
     return array[randomIndex];
 }
 
-function getNewsText(language) {
-    return '+++ ' + getRandom(news[language]) + ' +++';
-}
+getNewsText = function(language) { return '+++ ' + getRandom(news[language]) + ' +++' }
 
 function setMarquee(language) {
     var marquee = document.getElementById("newsticker-marquee");
@@ -31,6 +35,4 @@ function setMarquee(language) {
     marquee.start();
 }
 
-function init(language) {
-    setMarquee(language);
-}
+init = setMarquee;
