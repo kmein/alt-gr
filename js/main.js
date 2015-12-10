a0ff9f2199645cabe7e2d7c603f09234daa04ccd
kmein <kieran.meinhardt@gmail.com>
2015-12-10 22:19:52 +0100

    Sehenswertes-Seite fertiggestellt.

--- js/main.js
+++ js/main.js
@@ -4,21 +4,24 @@ var news = {
         "Sokrates: er weiß, dass er nichts weiß", 
         "Hokus Pokus Phidias: Zeusstatue in Olympia fertiggestellt",
         "Platon: 'Atlantis 480 BC' läuft in den Kinos an",
-        "Sokrates: zum Tode verurteilt"
+        "Sokrates: zum Tode verurteilt",
+        "Lesbos: Daphnis und Chloe in Busch entdeckt"
      ],
     'LA': [
         "Tróia: cives in via lígnea", 
         "Sócrates: scit se nescire", 
         "Hocus pocus Phídias: simulacrum Iovis confectum in Olýmpia",
         "Plato: 'Atlantia 480 a. Chr. n.' incípitur in theatris",
-        "Sócrates: capitis est damnatus"
+        "Sócrates: capitis est damnatus",
+        "Lesbus: Daphis et Chloe visi sunt in dumeto"
     ],
     'GR': [
         "Τροία: αἱ πολιταὶ ἐφ' ὁδῷ ξυλινῷ", 
         "Σωκράτης: ἓν οἶδεν ὄτι οὐδὲν οἶδεν", 
         "Ὅκος πόκος Φειδίας: εἴδωλον τοῦ Διὸς διαπέπρακται ἐν τῇ Ὀλυμπίᾳ",
         "Πλάτων: 'Ἀτλαντίς 480 π. Χρ.' ἄρχεται ἐν τοῖς θεάτροις",
-        "Σωκράτης: κατεγνοῦται θανάτου"
+        "Σωκράτης: κατεγνοῦται θανάτου",
+        "Λεσβός: ὁ Δάφνις καὶ ἡ Χλοὴ εἴδοντο ἐν ἕρκῳ"
     ]
 };
 
